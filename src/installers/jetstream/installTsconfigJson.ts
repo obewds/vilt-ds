1dc47af6254e40126c9b0ea53dbf483ef211955d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 19:58:17 -0400

    adding new ./src/installers/installTailwindConfigTs.ts

---
+++ src/installers/jetstream/installTsconfigJson.ts
@@ -0,0 +1,30 @@
+// ./src/installers/installTsconfigJson.ts
+
+import fs from 'fs-extra'
+
+import generateJetstreamTypescriptConfig from '../../generators/jetstream/generateJetstreamTypescriptConfig.js'
+
+import cliSuccessMessage from '../../helpers/cliSuccessMessage.js'
+import cliWarningMessage from '../../helpers/cliWarningMessage.js'
+import cwd from '../../helpers/cwd.js'
+import projectFileDoesExist from '../../helpers/projectFileDoesExist.js'
+
+export default function (): void {
+
+    const filename = 'vite.config.ts'
+    const filepath = '/' + filename
+
+    if ( ! projectFileDoesExist(filepath) ) {
+
+        fs.outputFileSync(cwd + filepath, generateJetstreamTypescriptConfig(), { flag: 'w+' })
+
+        cliSuccessMessage(filepath + ' file was created successfully!', false, false)
+
+    } else {
+
+        cliWarningMessage(filepath + ' was found, so no changes were made!', false, false)
+
+    }
+
+
+}
