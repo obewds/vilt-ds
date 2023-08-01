09daf9bb66feec71b088efa6f02b213626e15500
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 14:19:22 -0400

    adding new ./cli/helpers/cliSuccessMessage.ts

---
+++ src/helpers/cliSuccessMessage.ts
@@ -0,0 +1,17 @@
+// ./cli/helpers/cliSuccessMessage.ts
+
+import gradientText from './gradientText.js'
+
+export default function (string:string, prefixLineBreak:boolean = false, suffixLineBreak:boolean = false): void {
+
+    if (prefixLineBreak) {
+        console.log(' ')
+    }
+
+    console.log(gradientText(string))
+
+    if (suffixLineBreak) {
+        console.log(' ')
+    }
+
+}
