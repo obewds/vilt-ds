cbfe21ee5e59c1f16d4ce35da99a15dc2bc419b0
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 19:57:55 -0400

    adding new ./src/data/npm/viltDsJetstreamDeps.ts

---
+++ src/data/npm/viltDsJetstreamDeps.ts
@@ -0,0 +1,29 @@
+// ./src/data/npm/viltDsJetstreamDeps.ts
+
+export type ViltDsJetstreamDependency = 'typescript'
+| '@types/node'
+| '@inertiajs/progress'
+| '@types/ziggy-js'
+| 'ziggy-js'
+| '@obewds/vueventus'
+| '@headlessui/vue'
+| '@tailwindcss/aspect-ratio'
+| '@headlessui/tailwindcss'
+| 'tailwind-scrollbar'
+| '@types/gsap'
+| 'gsap'
+
+export default [
+    'typescript',
+    '@types/node',
+    '@inertiajs/progress',
+    '@types/ziggy-js',
+    'ziggy-js',
+    '@obewds/vueventus',
+    '@headlessui/vue',
+    '@tailwindcss/aspect-ratio',
+    '@headlessui/tailwindcss',
+    'tailwind-scrollbar',
+    '@types/gsap',
+    'gsap',
+] as Array<ViltDsJetstreamDependency>
