b67a9c88780fc73af52d51d08b9a7e00745e0c72
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 19:59:15 -0400

    adding new ./src/updaters/jetstream/updateRoutesWebPhp.ts

---
+++ src/updaters/jetstream/updateRoutesWebPhp.ts
@@ -0,0 +1,58 @@
+// ./src/updaters/jetstream/updateRoutesWebPhp.ts
+
+import fs from 'fs-extra'
+
+import cliSuccessMessage from '../../helpers/cliSuccessMessage.js'
+import cliWarningMessage from '../../helpers/cliWarningMessage.js'
+import cwd from '../../helpers/cwd.js'
+import projectFileDoesExist from '../../helpers/projectFileDoesExist.js'
+
+const newRoutesWebPhpCode = `});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('/vueventus', function () {
+        return Inertia::render('VueVentus');
+    })->name('vueventus');
+
+});`
+
+export default function (): void {
+
+    const filename = 'web.php'
+    const filepath = '/routes/' + filename
+
+    if ( projectFileDoesExist(filepath) ) {
+
+        fs.readFile(cwd + filepath, 'utf8', function(err, data) {
+            
+            // error check
+            if (err) { throw err }
+
+            let modifiedData = String(data).replace(
+                '});',
+                newRoutesWebPhpCode
+            )
+            
+            // If no error, then overwrite the current config/jetstream.php file content with the modifications
+            fs.outputFileSync(cwd + filepath, modifiedData, { flag: 'w+' })
+
+            cliSuccessMessage(cwd + filepath + ' file updated successfully!', false, false)
+
+        })
+
+    } else {
+
+        cliWarningMessage(cwd + filepath + ' not found, so no updates were made!', false, false)
+
+    }
+
+}
