af42b58936d8debbd7bf85c45a34ecfc5a7070ea
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 10:38:40 -0400

    generated fresh dist files to test locally for ts errors

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 // ./src/index.ts
-import gradientText from './helpers/gradientText.js';
-// show cli start message
-console.log(`
-
-
-    ${gradientText('Welcome to the VILT DS CLI!')}
-    ${gradientText('---------------------------')}
-    Install VILT stack starter files with VILT DS powered by VueVentus.
-
-
-`);
+import cliStartMessage from './helpers/cliStartMessage.js';
+import installViltDsJetstream from './installers/installViltDsJetstream.js';
+cliStartMessage();
+installViltDsJetstream();
 //# sourceMappingURL=index.js.map
---
+++ dist/installers/installViltDsJetstream.js
@@ -0,0 +1,6 @@
+// ./src/installers/installViltDsJetstream.ts
+import gitignore from '../generators/jetstream/gitignore.js';
+export default function () {
+    console.log(gitignore);
+}
+//# sourceMappingURL=installViltDsJetstream.js.map
