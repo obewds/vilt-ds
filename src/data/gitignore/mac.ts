ed29540e48a91b4c67e55947b459447926c34ba4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 13:22:11 -0400

    adding new ./src/data/gitignore/mac.ts file/data

---
+++ src/data/gitignore/mac.ts
@@ -0,0 +1,25 @@
+// ./src/data/gitignore/mac.ts
+
+export default `# MacOS General
+.DS_Store
+.AppleDouble
+.LSOverride
+
+# MacOS Thumbnails
+._*
+
+# MacOS Files that might appear in the root of a volume
+.DocumentRevisions-V100
+.fseventsd
+.Spotlight-V100
+.TemporaryItems
+.Trashes
+.VolumeIcon.icns
+.com.apple.timemachine.donotpresent
+
+# MacOS Directories potentially created on remote AFP share
+.AppleDB
+.AppleDesktop
+Network Trash Folder
+Temporary Items
+.apdisk`
