21d5978d6bf1349f7bcdc305f01f7f86e735553c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 08:59:58 -0400

    adding in an initial index.ts file with code using gradientText helper to test downstream use

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+// ./src/index.ts
+
+import gradientText from './helpers/gradientText.js'
+
+// show cli start message
+console.log(`
+
+
+    ${gradientText('Welcome to the VILT DS CLI!')}
+    ${gradientText('---------------------------')}
+    Install VILT stack starter files with VILT DS powered by VueVentus.
+
+
+`)
