95eaa35f7695f73236858d55a26cde2fcd6dc7ba
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 17:42:30 -0400

    adding new ./src/data/npm/jetstreamNpmDeps.ts

---
+++ src/data/npm/jetstreamNpmDeps.ts
@@ -0,0 +1,29 @@
+// ./src/data/npm/jetstreamNpmDeps.ts
+
+export type JetstreamNpmDependency = 'typescript'
+| '@types/node'
+| '@inertiajs/progress'
+| '@types/ziggy-js'
+| 'ziggy-js'
+| '@obewds/vueventus'
+| '@headlessui/vue'
+| '@tailwindcss/aspect-ratio'
+| '@headlessui/tailwindcss'
+| 'tailwind-scrollbar'
+| '@types/gsap'
+| 'gsap'
+
+export default [
+    'typescript',
+    '@types/node',
+    '@inertiajs/progress',
+    '@types/ziggy-js',
+    'ziggy-js',
+    '@obewds/vueventus',
+    '@headlessui/vue',
+    '@tailwindcss/aspect-ratio',
+    '@headlessui/tailwindcss',
+    'tailwind-scrollbar',
+    '@types/gsap',
+    'gsap',
+] as Array<JetstreamNpmDependency>
