b251d31b7c916cc0663b25177b86b1adab14c285
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 13:11:09 -0400

    adding new ./src/generators/jetstream/generateViteWindowDts.ts

---
+++ src/generators/jetstream/generateViteWindowDts.ts
@@ -0,0 +1,24 @@
+// ./src/generators/jetstream/generateViteWindowDts.ts
+
+export default function (): string {
+
+return `// ./resources/js/vite-window.d.ts
+
+import type { AxiosInstance } from 'axios'
+import type { Config, InputParams, Router } from 'ziggy-js'
+
+declare global {
+
+    interface Window {
+        _: Lodash,
+        axios: AxiosInstance
+        "Ziggy": Config
+    }
+
+    declare function route(): Router;
+    declare function route(name: string, params?: InputParams, absolute?: boolean, customZiggy?: Config): string;
+
+}
+`;
+
+}
