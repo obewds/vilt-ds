be2a1a47f308cb97b091739366b7ee7df58acc0e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 23:32:43 -0400

    adding new ./src/helpers/installWhenNoFile.ts

---
+++ src/helpers/installWhenNoFile.ts
@@ -0,0 +1,27 @@
+// ./src/helpers/installWhenNoFile.ts
+
+import fs from 'fs-extra'
+
+import cliSuccessMessage from './cliSuccessMessage.js'
+import cliWarningMessage from './cliWarningMessage.js'
+import cwd from './cwd.js'
+import projectFileDoesExist from './projectFileDoesExist.js'
+
+export default function (rootRelativePath:string, filenameStr:string, newFileData:string): void {
+
+    const filename = filenameStr
+    const filepath = rootRelativePath + filename
+
+    if ( ! projectFileDoesExist(filepath) ) {
+
+        fs.outputFileSync(cwd + filepath, newFileData, { flag: 'w+' })
+
+        cliSuccessMessage('.' + filepath + ' file was created successfully!', false, false)
+
+    } else {
+
+        cliWarningMessage('A .' + filepath + ' file was found, so no changes were made!', false, false)
+
+    }
+
+}
