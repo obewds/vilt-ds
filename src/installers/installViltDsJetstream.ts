5b722a03cec049bec41ced6fff774295bc8e5b2f
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 13:12:35 -0400

    integrating new installers into ./src/installers/installViltDsJetstream.ts

--- src/installers/installViltDsJetstream.ts
+++ src/installers/installViltDsJetstream.ts
@@ -4,13 +4,19 @@ import installAppColorsJson from '../installers/jetstream/installAppColorsJson.j
 
 import installAppColorsTypesTs from '../installers/jetstream/installAppColorsTypesTs.js'
 import installAppJetstreamTypesTs from '../installers/jetstream/installAppJetstreamTypesTs.js'
+import installAppTs from '../installers/jetstream/installAppTs.js'
 import installAppTypesTs from '../installers/jetstream/installAppTypesTs.js'
+import installAppVvClassesTs from '../installers/jetstream/installAppVvClassesTs.js'
 import installAppVvClassesTypesTs from '../installers/jetstream/installAppVvClassesTypesTs.js'
+import installAppVvTs from '../installers/jetstream/installAppVvTs.js'
+import installBootstrapTs from '../installers/jetstream/installBootstrapTs.js'
 import installVscodeExtensionsJson from '../installers/jetstream/installVscodeExtensionsJson.js'
 import installNpmDeps from '../installers/jetstream/installNpmDeps.js'
 import installTailwindConfigTs from '../installers/jetstream/installTailwindConfigTs.js'
 import installTsconfigJson from '../installers/jetstream/installTsconfigJson.js'
 import installViteConfigTs from '../installers/jetstream/installViteConfigTs.js'
+import installViteTsxDts from '../installers/jetstream/installViteTsxDts.js'
+import installViteWindowDts from '../installers/jetstream/installViteWindowDts.js'
 
 import updateAppBladePhp from '../updaters/jetstream/updateAppBladePhp.js'
 import updateConfigJetstreamPhp from '../updaters/jetstream/updateConfigJetstreamPhp.js'
@@ -20,34 +26,45 @@ import updateRoutesWebPhp from '../updaters/jetstream/updateRoutesWebPhp.js'
 
 export default function (): void {
 
+    // update the app gitignore file for VILT DS
     updateGitignore()
 
+    // install client side deps for VILT DS / VueVentus
     installNpmDeps()
 
-    installVscodeExtensionsJson()
-
-    installTsconfigJson()
-
+    // install typescript specific app files
     installAppColorsTypesTs()
-    
     installAppJetstreamTypesTs()
-
     installAppTypesTs()
-
     installAppVvClassesTypesTs()
+    installTsconfigJson()
+    installViteTsxDts()
+    installViteWindowDts()
 
-    installAppColorsJson()
-
-    installTailwindConfigTs()
-
+    // install vue ecosystem app typed VILT DS files
+    installBootstrapTs()
+    installAppTs()
+    installAppVvTs()
     installViteConfigTs()
 
+    // install tailwind app typed files
+    installTailwindConfigTs()
+
+    // install VueVentus app files used by VILT DS
+    installAppColorsJson()
+    installAppVvClassesTs()
+
+    // install laravel blade files
     updateAppBladePhp()
 
+    // update laravel php files for VILT DS
     updateConfigJetstreamPhp()
-
     updateRoutesWebPhp()
 
+    // update the project package json file
     updatePackageJson()
+
+    // install suggested VS Code Extensions for VILT DS
+    installVscodeExtensionsJson()
 
 }
