b54e0fd6671903dc793e8afdbf63125952305ca0
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 13:10:12 -0400

    adding new ./src/generators/jetstream/generateAppTs.ts

---
+++ src/generators/jetstream/generateAppTs.ts
@@ -0,0 +1,45 @@
+// ./src/generators/jetstream/generateAppTs.ts
+
+export default function (): string {
+
+return `// ./resources/js/app.ts
+
+import type { DefineComponent } from 'vue'
+import type { CreateInertiaAppProps } from './Types/app-types'
+import type { VvAppGenericColors, VvAppDefaultTintsShades } from './Types/app-colors-types'
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.es'
+import { Ziggy as ziggyGeneratedData } from './ziggy'
+import appColorsJson from './app.colors.json'
+
+import './bootstrap'
+import '../css/app.css'
+import './gsap'
+
+const appName = import.meta.env.VITE_APP_NAME || 'No VITE_APP_NAME found'
+const appColors: VvAppGenericColors<VvAppDefaultTintsShades> = appColorsJson
+
+const inertiaProps: CreateInertiaAppProps = {
+    title: (title: string) => ` + "`${title} - ${appName}`" + `,
+    resolve: (name: string) => resolvePageComponent(
+        ` + "`./Pages/${name}.vue`" + `,
+        import.meta.glob<DefineComponent>('./Pages/**/*.vue')
+    ),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, ziggyGeneratedData)
+        .mount(el)
+    },
+    progress: {
+        color: appColors.blue["500"],
+    },
+}
+
+createInertiaApp(inertiaProps)
+`;
+
+}
