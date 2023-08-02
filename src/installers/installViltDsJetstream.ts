fd4e45982dff35ba8f81f6201ed0e246d1253784
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 18:50:48 -0400

    updating src/installers/installViltDsJetstream.ts to new kebob files

--- src/installers/installViltDsJetstream.ts
+++ src/installers/installViltDsJetstream.ts
@@ -1,7 +1,6 @@
 // ./src/installers/installViltDsJetstream.ts
 
 import installAppColorsJson from '../installers/jetstream/installAppColorsJson.js'
-
 import installAppColorsTypesTs from '../installers/jetstream/installAppColorsTypesTs.js'
 import installAppJetstreamTypesTs from '../installers/jetstream/installAppJetstreamTypesTs.js'
 import installAppTs from '../installers/jetstream/installAppTs.js'
@@ -10,19 +9,23 @@ import installAppVvClassesTs from '../installers/jetstream/installAppVvClassesTs
 import installAppVvClassesTypesTs from '../installers/jetstream/installAppVvClassesTypesTs.js'
 import installAppVvTs from '../installers/jetstream/installAppVvTs.js'
 import installBootstrapTs from '../installers/jetstream/installBootstrapTs.js'
-import installVscodeExtensionsJson from '../installers/jetstream/installVscodeExtensionsJson.js'
 import installNpmDeps from '../installers/jetstream/installNpmDeps.js'
 import installTailwindConfigTs from '../installers/jetstream/installTailwindConfigTs.js'
 import installTsconfigJson from '../installers/jetstream/installTsconfigJson.js'
 import installViteConfigTs from '../installers/jetstream/installViteConfigTs.js'
 import installViteTsxDts from '../installers/jetstream/installViteTsxDts.js'
 import installViteWindowDts from '../installers/jetstream/installViteWindowDts.js'
+import installVscodeExtensionsJson from '../installers/jetstream/installVscodeExtensionsJson.js'
 
 import updateAppBladePhp from '../updaters/jetstream/updateAppBladePhp.js'
 import updateConfigJetstreamPhp from '../updaters/jetstream/updateConfigJetstreamPhp.js'
 import updateGitignore from '../updaters/jetstream/updateGitignore.js'
 import updatePackageJson from '../updaters/jetstream/updatePackageJson.js'
 import updateRoutesWebPhp from '../updaters/jetstream/updateRoutesWebPhp.js'
+
+
+import installJetstreamGsapTs from './jetstream/resources/js/install-jetstream-gsap-ts.js'
+
 
 export default function (): void {
 
@@ -50,9 +53,10 @@ export default function (): void {
     // install tailwind app typed files
     installTailwindConfigTs()
 
-    // install VueVentus app files used by VILT DS
+    // install VueVentus and VueVentus dependency app files
     installAppColorsJson()
     installAppVvClassesTs()
+    installJetstreamGsapTs()
 
     // install laravel blade files
     updateAppBladePhp()
