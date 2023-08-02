cfb4fb39965b0f837a8c360ad11990f157458043
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 23:35:05 -0400

    converting installViteConfigTs.ts to use installWhenNoFile()

--- src/installers/jetstream/installViteConfigTs.ts
+++ src/installers/jetstream/installViteConfigTs.ts
@@ -1,30 +1,11 @@
-// ./src/installers/installViteConfigTs.ts
+// ./src/installers/jetstream/installViteConfigTs.ts
 
-import fs from 'fs-extra'
+import installWhenNoFile from '../../helpers/installWhenNoFile.js'
 
 import generateViteConfigTs from '../../generators/jetstream/generateViteConfigTs.js'
 
-import cliSuccessMessage from '../../helpers/cliSuccessMessage.js'
-import cliWarningMessage from '../../helpers/cliWarningMessage.js'
-import cwd from '../../helpers/cwd.js'
-import projectFileDoesExist from '../../helpers/projectFileDoesExist.js'
-
 export default function (): void {
 
-    const filename = 'vite.config.ts'
-    const filepath = '/' + filename
-
-    if ( ! projectFileDoesExist(filepath) ) {
-
-        fs.outputFileSync(cwd + filepath, generateViteConfigTs(), { flag: 'w+' })
-
-        cliSuccessMessage('.' + filepath + ' file was created successfully!', false, false)
-
-    } else {
-
-        cliWarningMessage('A .' + filepath + ' file was found, so no changes were made!', false, false)
-
-    }
-
+    installWhenNoFile( '/', 'vite.config.ts', generateViteConfigTs() )
 
 }
