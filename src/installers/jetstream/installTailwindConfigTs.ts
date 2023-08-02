696b54d2c67e9ee62f9c3262d9f8c484c7cbb771
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-01 23:34:43 -0400

    converting installTailwindConfigTs.ts to use installWhenNoFile()

--- src/installers/jetstream/installTailwindConfigTs.ts
+++ src/installers/jetstream/installTailwindConfigTs.ts
@@ -1,29 +1,11 @@
-// ./src/installers/installTailwindConfigTs.ts
+// ./src/installers/jetstream/installTailwindConfigTs.ts
 
-import fs from 'fs-extra'
+import installWhenNoFile from '../../helpers/installWhenNoFile.js'
 
 import generateTailwindConfigTs from '../../generators/jetstream/generateTailwindConfigTs.js'
 
-import cliSuccessMessage from '../../helpers/cliSuccessMessage.js'
-import cliWarningMessage from '../../helpers/cliWarningMessage.js'
-import cwd from '../../helpers/cwd.js'
-import projectFileDoesExist from '../../helpers/projectFileDoesExist.js'
-
 export default function (): void {
 
-    const filename = 'tailwind.config.ts'
-    const filepath = '/' + filename
-
-    if ( ! projectFileDoesExist(filepath) ) {
-
-        fs.outputFileSync(cwd + filepath, generateTailwindConfigTs(), { flag: 'w+' })
-
-        cliSuccessMessage('.' + filepath + ' file was created successfully!', false, false)
-
-    } else {
-
-        cliWarningMessage('A .' + filepath + ' file was found, so no changes were made!', false, false)
-
-    }
+    installWhenNoFile( '/', 'tailwind.config.ts', generateTailwindConfigTs() )
 
 }
