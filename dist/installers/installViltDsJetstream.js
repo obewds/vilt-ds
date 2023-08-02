10c598fb0d0cb6a46e084660b7796405d6f0f433
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 15:48:09 -0400

    generating fresh dist files for local test

--- dist/installers/installViltDsJetstream.js
+++ dist/installers/installViltDsJetstream.js
@@ -8,18 +8,19 @@ import installAppVvClassesTs from '../installers/jetstream/installAppVvClassesTs
 import installAppVvClassesTypesTs from '../installers/jetstream/installAppVvClassesTypesTs.js';
 import installAppVvTs from '../installers/jetstream/installAppVvTs.js';
 import installBootstrapTs from '../installers/jetstream/installBootstrapTs.js';
-import installVscodeExtensionsJson from '../installers/jetstream/installVscodeExtensionsJson.js';
 import installNpmDeps from '../installers/jetstream/installNpmDeps.js';
 import installTailwindConfigTs from '../installers/jetstream/installTailwindConfigTs.js';
 import installTsconfigJson from '../installers/jetstream/installTsconfigJson.js';
 import installViteConfigTs from '../installers/jetstream/installViteConfigTs.js';
 import installViteTsxDts from '../installers/jetstream/installViteTsxDts.js';
 import installViteWindowDts from '../installers/jetstream/installViteWindowDts.js';
+import installVscodeExtensionsJson from '../installers/jetstream/installVscodeExtensionsJson.js';
 import updateAppBladePhp from '../updaters/jetstream/updateAppBladePhp.js';
 import updateConfigJetstreamPhp from '../updaters/jetstream/updateConfigJetstreamPhp.js';
 import updateGitignore from '../updaters/jetstream/updateGitignore.js';
 import updatePackageJson from '../updaters/jetstream/updatePackageJson.js';
 import updateRoutesWebPhp from '../updaters/jetstream/updateRoutesWebPhp.js';
+import installJetstreamGsapTs from './jetstream/resources/js/install-jetstream-gsap-ts.js';
 export default function () {
     // update the app gitignore file for VILT DS
     updateGitignore();
@@ -40,9 +41,10 @@ export default function () {
     installViteConfigTs();
     // install tailwind app typed files
     installTailwindConfigTs();
-    // install VueVentus app files used by VILT DS
+    // install VueVentus and VueVentus dependency app files
     installAppColorsJson();
     installAppVvClassesTs();
+    installJetstreamGsapTs();
     // install laravel blade files
     updateAppBladePhp();
     // update laravel php files for VILT DS
