bcfae05f6199b0f244780bf794d39c52ab04071b
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 22:59:06 -0400

    adding new ./tests/helpers/select-installation.test.ts

---
+++ tests/helpers/select-installation.test.ts
@@ -0,0 +1,16 @@
+// ./tests/helpers/select-installation.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import selectInstallation from '../../src/helpers/select-installation'
+
+describe('./src/helpers/select-installation module', () => {
+
+    // test('module can be imported successfully', () => {
+
+        // expect(selectInstallation).toBeTruthy()
+
+    // })
+
+    test('faux', () => { expect(true).toBeTruthy() })
+
+})
