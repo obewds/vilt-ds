8af6280d7b10f9b5a7ef6ae44f0a600b259bb4cc
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 22:55:56 -0400

    adding new ./src/helpers/project-file-does-exist.ts

---
+++ src/helpers/project-file-does-exist.ts
@@ -0,0 +1,20 @@
+// ./src/helpers/project-file-does-exist.ts
+
+import fs from 'fs-extra'
+
+import cwd from './cwd.js'
+
+export default function (filepath:string): boolean {
+
+    const currentFilepath = cwd + filepath
+
+    try {
+        if (fs.existsSync(currentFilepath)) {
+            return true
+        }
+    } catch(err) {
+        console.error(err)
+        return false
+    }
+
+}
