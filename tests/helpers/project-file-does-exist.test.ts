f01397b8b9270581a0b670f619d9fc137845481f
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-03 16:48:09 -0400

    improving ./tests/helpers/project-file-does-exist.test.ts test coverage

--- tests/helpers/project-file-does-exist.test.ts
+++ tests/helpers/project-file-does-exist.test.ts
@@ -11,4 +11,28 @@ describe('./src/helpers/project-file-does-exist module', () => {
 
     })
 
+    test('module returns a false value for a file that does not exist', async () => {
+
+        const result = projectFileDoesExist('/file-that-does-not-exist.js')
+
+        expect(result).toBe(false)
+
+    })
+
+    test('module returns a false value for an empty string argument', async () => {
+
+        const result = projectFileDoesExist('')
+
+        expect(result).toBe(false)
+
+    })
+
+    test('module returns a true value for an existing file matching the string argument', async () => {
+
+        const result = projectFileDoesExist('/package.json')
+
+        expect(result).toBe(true)
+
+    })
+
 })
