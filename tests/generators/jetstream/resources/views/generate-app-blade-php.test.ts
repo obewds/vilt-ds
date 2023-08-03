8aef646254cfb41f81921f57d21c75c69436f983
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 22:58:22 -0400

    adding new ./tests/generators/jetstream/resources/views/generate-app-blade-php.ts

---
+++ tests/generators/jetstream/resources/views/generate-app-blade-php.test.ts
@@ -0,0 +1,15 @@
+// ./tests/generators/jetstream/resources/views/generate-app-blade-php.ts
+
+import { describe, expect, test } from '@jest/globals'
+
+import generateAppTs from '../../../../../src/generators/jetstream/resources/views/generate-app-blade-php'
+
+describe('./src/generators/jetstream/resources/views/generate-app-blade-ph module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(generateAppTs).toBeTruthy()
+
+    })
+
+})
