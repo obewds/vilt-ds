32ea67bf187ff9db5ab5aa8870482a5f92827817
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-03 17:24:39 -0400

    adding new ./src/generators/jetstream/resources/js/generate-vite-tsx-d-ts.ts

---
+++ src/generators/jetstream/resources/js/generate-vite-tsx-d-ts.ts
@@ -0,0 +1,23 @@
+// ./src/generators/jetstream/resources/js/generate-vite-tsx-d-ts.ts
+
+export default function (): string {
+
+return `// ./resources/js/vite-tsx.d.ts
+
+import type { VNode } from 'vue'
+import Vue from 'vue'
+
+declare global {
+    namespace JSX {
+        // tslint:disable no-empty-interface
+        interface Element extends VNode {}
+        // tslint:disable no-empty-interface
+        interface ElementClass extends Vue {}
+        interface IntrinsicElements {
+            [elem: string]: any
+        }
+    }
+}
+`;
+
+}
