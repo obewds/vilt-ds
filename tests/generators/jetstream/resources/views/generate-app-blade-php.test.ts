c38184055aba7cb959522606c53c5d0771f848b4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-02 23:18:33 -0400

    adding test for returned valid string to generate-app-blade-php.test.ts

--- tests/generators/jetstream/resources/views/generate-app-blade-php.test.ts
+++ tests/generators/jetstream/resources/views/generate-app-blade-php.test.ts
@@ -1,14 +1,20 @@
-// ./tests/generators/jetstream/resources/views/generate-app-blade-php.ts
+// ./tests/generators/jetstream/resources/views/generate-app-blade-php.test.ts
 
 import { describe, expect, test } from '@jest/globals'
 
-import generateAppTs from '../../../../../src/generators/jetstream/resources/views/generate-app-blade-php'
+import generateAppBladePhp from '../../../../../src/generators/jetstream/resources/views/generate-app-blade-php'
 
 describe('./src/generators/jetstream/resources/views/generate-app-blade-ph module', () => {
 
     test('module can be imported successfully', () => {
 
-        expect(generateAppTs).toBeTruthy()
+        expect(generateAppBladePhp).toBeTruthy()
+
+    })
+
+    test('module default value is a valid string', () => {
+
+        expect(typeof generateAppBladePhp() === 'string').toBe(true)
 
     })
 
