ab68b39616d16c97a0b1ebdf062d609f4ab9997d
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-03 18:24:51 -0400

    adding new ./src/generators/jetstream/generate-gitignore.ts

---
+++ src/generators/jetstream/generate-gitignore.ts
@@ -0,0 +1,20 @@
+// ./src/generators/jetstream/generate-gitignore.ts
+
+import viltDsDeveloperGitignore from '../../data/gitignore/vilt-ds-developer-gitignore.js'
+import viltDsMacGitignore from '../../data/gitignore/vilt-ds-mac-gitignore.js'
+import viltDsWindowsGitignore from '../../data/gitignore/vilt-ds-windows-gitignore.js'
+
+export default function (installedGitignoreText:string): string {
+
+return `# ./.gitignore
+
+${installedGitignoreText}
+
+${viltDsDeveloperGitignore}
+
+${viltDsMacGitignore}
+
+${viltDsWindowsGitignore}
+`;
+
+}
