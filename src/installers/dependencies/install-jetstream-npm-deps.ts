e57d1a1ec592298ef72c4a152db4525e27317560
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-04 13:16:16 -0400

    adding new ./src/installers/dependencies/install-jetstream-npm-deps.ts

---
+++ src/installers/dependencies/install-jetstream-npm-deps.ts
@@ -0,0 +1,20 @@
+// ./src/installers/dependencies/install-jetstream-npm-deps.ts
+
+import cliSuccessMessage from '../../helpers/cli-success-message.js'
+import run from '../../helpers/run.js'
+
+import viltDsJetstreamDeps from '../../data/npm/vilt-ds-jetstream-deps.js'
+
+export default function (): void {
+
+    const command = 'npm install -D ' + viltDsJetstreamDeps.join(' ')
+
+    run(command)
+
+    console.log(' ')
+
+    viltDsJetstreamDeps.forEach( (dep) => cliSuccessMessage(dep + ' dependency installed!', false, false))
+
+    console.log(' ')
+
+}
