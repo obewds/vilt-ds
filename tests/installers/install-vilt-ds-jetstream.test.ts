8f062e180526198be833dd1dda8d7d803d726f1a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-04 16:44:33 -0400

    adding new ./tests/installers/install-vilt-ds-jetstream.test.ts

---
+++ tests/installers/install-vilt-ds-jetstream.test.ts
@@ -0,0 +1,14 @@
+// ./tests/installers/install-vilt-ds-jetstream.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import installViltDsJetstream from '../../src/installers/install-vilt-ds-jetstream'
+
+describe('./src/installers/install-vilt-ds-jetstream module', () => {
+
+    test('module can be imported and a value can be accessed successfully', () => {
+
+        expect(installViltDsJetstream).toBeTruthy()
+
+    })
+
+})
