fb5c74ba650c6b9752ba16f68299b8e8ca8fb0b4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-04 16:44:22 -0400

    adding new ./tests/generators/jetstream/generate-web-php.test.ts

---
+++ tests/generators/jetstream/routes/generate-web-php.test.ts
@@ -0,0 +1,20 @@
+// ./tests/generators/jetstream/generate-web-php.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import generateWebPhp from '../../../../src/generators/jetstream/routes/generate-web-php'
+
+describe('./src/generators/jetstream/vscode/generate-web-php.test module', () => {
+
+    test('module can be imported and a value can be accessed successfully', () => {
+
+        expect(generateWebPhp).toBeTruthy()
+
+    })
+    
+    test('module default value is a valid string', () => {
+
+        expect(typeof generateWebPhp() === 'string').toBe(true)
+
+    })
+
+})
