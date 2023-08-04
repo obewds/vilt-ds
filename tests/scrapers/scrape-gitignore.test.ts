677eb1fe5f2de21f5ea957eaebc85ae9c17eb2fe
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-04 15:58:29 -0400

    adding new ./tests/scrapers/scrape-gitignore.test.ts

---
+++ tests/scrapers/scrape-gitignore.test.ts
@@ -0,0 +1,22 @@
+// ./tests/scrapers/scrape-gitignore.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import fs from 'fs-extra'
+import cwd from '../../src/helpers/cwd'
+import scrapeGitignore from '../../src/scrapers/scrape-gitignore'
+
+describe('./src/scrapers/scrape-gitignore module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(scrapeGitignore).toBeTruthy()
+
+    })
+
+    test('module returns null when a config/jetstream.php file does not exist', () => {
+
+        expect(scrapeGitignore()).toBe(null)
+
+    })
+
+})
