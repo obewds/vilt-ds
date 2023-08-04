faf8ddb943fa34e744df0128532192a0a79930e9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-04 10:35:55 -0400

    adding new ./src/scrapers/scrape-gitignore.ts

---
+++ src/scrapers/scrape-gitignore.ts
@@ -0,0 +1,33 @@
+// ./src/scrapers/scrape-gitignore.ts
+
+import fs from 'fs-extra'
+import cwd from '../helpers/cwd.js'
+import projectFileDoesExist from '../helpers/project-file-does-exist.js'
+
+export default function (): string|null {
+
+    const path = cwd + '/.gitignore'
+
+    const fileExists = projectFileDoesExist(path)
+
+    if (fileExists) {
+
+        var scraped = fs.readFileSync(path,'utf8')
+
+        let modifiedData = String(scraped).replace(
+            '// Features::termsAndPrivacyPolicy()',
+            'Features::termsAndPrivacyPolicy()'
+        ).replace(
+            '// Features::api()',
+            'Features::api()'
+        )
+
+        return modifiedData
+
+    } else {
+
+        return null
+
+    }
+
+}
