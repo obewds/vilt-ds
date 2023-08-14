7aed82669f22dfe949a5e84ddc654733e03a7487
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-14 08:16:50 -0400

    adding new ./src/generators/jetstream/resources/js/Components/generate-action-message-vue.ts

---
+++ src/generators/jetstream/resources/js/Components/generate-action-message-vue.ts
@@ -0,0 +1,62 @@
+// ./src/generators/jetstream/resources/js/Components/generate-action-message-vue.ts
+
+export default function (): string {
+
+return `<!-- ./resources/js/Components/ActionMessage.vue -->
+
+<script lang="ts">
+
+    import type { PropType } from 'vue'
+    import type { DefaultPaletteColors, DefaultPalettes } from '@obewds/vueventus'
+
+    import { defineComponent } from 'vue'
+    import VvEl from './vv/elements/VvEl.vue'
+
+    export default defineComponent({
+
+        components: { VvEl },
+
+        props: {
+            on: {
+                type: Boolean,
+                default: false,
+            },
+            textColor: {
+                type: String as PropType<keyof DefaultPaletteColors>,
+                default: 'success',
+            },
+            textPalette: {
+                type: String as PropType<keyof DefaultPalettes>,
+                default: 'default',
+            },
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <div>
+
+        <transition
+            leave-active-class="transition ease-in duration-1000"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <VvEl
+                v-show="on"
+                size="sm"
+                :text-palette="textPalette"
+                :text-color="textColor"
+            >
+                <slot />
+            </VvEl>
+        </transition>
+
+    </div>
+
+</template>
+`;
+
+}
