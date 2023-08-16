bc81936d7a31fac91d96ad6e19bcb3bf51f5a5cc
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-16 00:08:00 -0400

    local build test generated dist files

---
+++ dist/generators/jetstream/resources/js/Components/generate-primary-button-vue.js
@@ -0,0 +1,38 @@
+// ./src/generators/jetstream/resources/js/Components/generate-primary-button-vue.ts
+export default function () {
+    return `<!-- ./resources/js/Components/PrimaryButton.vue -->
+
+<script lang="ts">
+
+    import type { PropType } from 'vue'
+
+    import { defineComponent } from 'vue'
+    import { ValidButtonTypes } from '@obewds/vueventus'
+
+    export default defineComponent({
+
+        props: {
+            type: {
+                type: String as PropType<ValidButtonTypes>,
+                default: 'submit',
+                validator: (prop: ValidButtonTypes) => (ValidButtonTypes).includes(prop),
+            },
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <button
+        :type="type"
+        class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+    >
+        <slot />
+    </button>
+
+</template>
+`;
+}
+//# sourceMappingURL=generate-primary-button-vue.js.map
---
+++ dist/generators/jetstream/resources/js/Components/generate-scroll-up-color-mode-section-vue.js
@@ -0,0 +1,54 @@
+// ./src/generators/jetstream/resources/js/Components/generate-scroll-up-color-mode-section-vue.ts
+export default function () {
+    return `<!-- ./resources/js/Components/ScrollUpColorModeSection.vue -->
+
+<script lang="ts">
+
+    import type { ValidColorModes } from '@obewds/vueventus'
+
+    import { defineComponent } from 'vue'
+    import { VvColorModeButton } from '@obewds/vueventus'
+    import VvScrollUp from '../Components/vv/buttons/VvScrollUp.vue'
+
+    export default defineComponent({
+
+        components: {
+            VvColorModeButton,
+            VvScrollUp,
+        },
+
+        setup () {
+
+            const colorMode = typeof window !== 'undefined' && localStorage && localStorage.getItem('colorMode') ? localStorage.getItem('colorMode') as ValidColorModes : 'light' as ValidColorModes
+
+            return {
+                colorMode,
+            }
+        },
+
+    })
+
+</script>
+
+<template>
+
+    <aside class="fixed bottom-0 right-0 text-right z-30">
+
+        <nav class="relative bottom-0 pb-3 pr-3">
+
+            <div class="flex flex-col justify-end space-y-3">
+
+                <VvScrollUp palette="solid" color="primary"/>
+
+                <VvColorModeButton palette="solid" color="primary" :mode="colorMode"/>
+
+            </div>
+
+        </nav>
+
+    </aside>
+
+</template>
+`;
+}
+//# sourceMappingURL=generate-scroll-up-color-mode-section-vue.js.map
