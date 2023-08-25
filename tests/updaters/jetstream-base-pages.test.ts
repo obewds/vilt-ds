84b1e86ad6ed642cf0b7a4bf60a11fee39ef1b00
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-25 00:14:32 -0400

    adding new ./tests/updaters/jetstream-base-pages.test.ts

---
+++ tests/updaters/jetstream-base-pages.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/jetstream-base-pages.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import jetstreamBasePages from '../../src/updaters/jetstream-base-pages'
+
+
+describe('./src/updaters/jetstream-base-pages module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(jetstreamBasePages).toBeTruthy()
+
+    })
+
+})
