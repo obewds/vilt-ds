ae658d56c352049d795e040f2ffd6b1fdbe954c3
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-24 22:55:41 -0400

    removing ./tests/updaters/jetstream-teams-components.test.ts

---
+++ tests/updaters/jetstream-team-components.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/jetstream-teams-components.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import jetstreamTeamComponents from '../../src/updaters/jetstream-teams-components'
+
+
+describe('./src/updaters/jetstream-teams-components module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(jetstreamTeamComponents).toBeTruthy()
+
+    })
+
+})
