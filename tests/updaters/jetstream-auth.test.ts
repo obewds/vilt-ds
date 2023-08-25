8d0cfa115939fe6720187bc59cf946e5334e2d7e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-25 00:14:11 -0400

    adding new ./tests/updaters/jetstream-auth.test.ts

---
+++ tests/updaters/jetstream-auth.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/jetstream-auth.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import jetstreamAuth from '../../src/updaters/jetstream-auth'
+
+
+describe('./src/updaters/jetstream-auth module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(jetstreamAuth).toBeTruthy()
+
+    })
+
+})
