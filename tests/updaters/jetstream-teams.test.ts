8ba6bc22e223cea8437090fa879c39f16d0f74d2
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-24 23:03:52 -0400

    adding new ./tests/updaters/jetstream-teams.test.ts

---
+++ tests/updaters/jetstream-teams.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/jetstream-teams.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import jetstreamTeams from '../../src/updaters/jetstream-teams'
+
+
+describe('./src/updaters/jetstream-teams module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(jetstreamTeams).toBeTruthy()
+
+    })
+
+})
