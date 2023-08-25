a17964bde592555a2c8fbb755e7a0690e0d23288
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-25 00:14:42 -0400

    adding new ./tests/updaters/jetstream-profile.test.ts

---
+++ tests/updaters/jetstream-profile.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/jetstream-profile.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import jetstreamProfile from '../../src/updaters/jetstream-profile'
+
+
+describe('./src/updaters/jetstream-profile module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(jetstreamProfile).toBeTruthy()
+
+    })
+
+})
