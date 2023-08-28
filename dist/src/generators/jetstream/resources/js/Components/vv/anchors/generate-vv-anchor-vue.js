9506d4d86aca5a5bff625849f1adf875f1b01aca
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-28 18:40:38 -0400

    generated fresh dist files to test build locally

---
+++ dist/src/generators/jetstream/resources/js/Components/vv/anchors/generate-vv-anchor-vue.js
@@ -0,0 +1,101 @@
+// ./src/generators/jetstream/resources/js/Components/vv/anchors/generate-vv-anchor-vue.ts
+import { generateVvAnchorVue } from '@obewds/puff-js/dist/index.js';
+export default function () {
+    return generateVvAnchorVue({
+        filePathComment: './resources/js/Components/vv/anchors/VvAnchor.vue',
+        appVvFilePath: '../../../app.vv',
+    });
+    /*
+    return `<!-- ./resources/js/Components/vv/anchors/VvAnchor.vue -->
+    
+    <script lang="ts">
+    
+        import type { PropType } from 'vue'
+        import type { DefaultButtonPalettes, DefaultPaletteColors, DefaultPalettes, SizesButtons, SizesText } from '@obewds/vueventus'
+    
+        import { defineComponent } from 'vue'
+        import { VvAnchor as LibVvAnchor } from '@obewds/vueventus'
+        import appVv from '../../../app.vv'
+    
+        export default defineComponent({
+    
+            components: { LibVvAnchor },
+    
+            props: {
+                button: {
+                    type: Boolean,
+                    default: appVv.defaults.VvAnchor.button,
+                },
+                buttonBlock: {
+                    type: Boolean,
+                    default: appVv.defaults.VvAnchor.buttonBlock,
+                },
+                buttonFab: {
+                    type: Boolean,
+                    default: appVv.defaults.VvAnchor.buttonFab,
+                },
+                buttonSize: {
+                    type: String as PropType<keyof SizesButtons>,
+                    default: appVv.defaults.VvAnchor.buttonSize,
+                },
+                color: {
+                    type: String as PropType<keyof DefaultPaletteColors>,
+                    default: appVv.defaults.VvAnchor.color,
+                },
+                debug: {
+                    type: Boolean,
+                    default: appVv.defaults.VvAnchor.debug,
+                },
+                external: {
+                    type: Boolean,
+                    default: appVv.defaults.VvAnchor.external,
+                },
+                href: {
+                    type: String,
+                    default: appVv.defaults.VvAnchor.href,
+                },
+                palette: {
+                    type: String as PropType<keyof DefaultPalettes | keyof DefaultButtonPalettes>,
+                    default: appVv.defaults.VvAnchor.palette,
+                },
+                textSize: {
+                    type: String as PropType<keyof SizesText>,
+                    default: appVv.defaults.VvAnchor.textSize,
+                },
+            },
+    
+        })
+    
+    </script>
+    
+    <template>
+    
+        <LibVvAnchor
+            :button="button"
+            :button-block="buttonBlock"
+            :button-fab="buttonFab"
+            :button-size="buttonSize"
+            :color="color"
+            :debug="debug"
+            :external="external"
+            :href="href"
+            :palette="palette"
+            :text-size="textSize"
+            :data-vv-anchor-prop-button="debug ? button : null"
+            :data-vv-anchor-prop-button-block="debug ? buttonBlock : null"
+            :data-vv-anchor-prop-button-fab="debug ? buttonFab : null"
+            :data-vv-anchor-prop-button-size="debug ? buttonSize : null"
+            :data-vv-anchor-prop-color="debug ? color : null"
+            :data-vv-anchor-prop-external="debug ? external : null"
+            :data-vv-anchor-prop-href="debug ? href : null"
+            :data-vv-anchor-prop-palette="debug ? palette : null"
+            :data-vv-anchor-prop-text-size="debug ? textSize : null"
+        >
+            <slot/>
+        </LibVvAnchor>
+    
+    </template>
+    `;
+    */
+}
+//# sourceMappingURL=generate-vv-anchor-vue.js.map
---
+++ dist/src/installers/install-vilt-ds-jetstream.js
@@ -0,0 +1,97 @@
+// ./src/installers/install-vilt-ds-jetstream.ts
+import installFile from '../helpers/install-file.js';
+import removeFile from '../helpers/remove-file.js';
+// import client side deps and package scripts/mods for VILT DS and VueVentus
+import installJetstreamNpmDeps from '../installers/dependencies/install-jetstream-npm-deps.js';
+import installJetstreamPackageJsonMods from '../installers/dependencies/install-jetstream-package-json-mods.js';
+// import the app gitignore file for VILT DS
+import generateGitignore from '../generators/jetstream/generate-gitignore.js';
+// import typescript specific app files
+import generateAppColorsTypesTs from '../generators/jetstream/resources/js/Types/generate-app-colors-types-ts.js';
+import generateAppJetstreamTypesTs from '../generators/jetstream/resources/js/Types/generate-app-jetstream-types-ts.js';
+import generateAppTypesTs from '../generators/jetstream/resources/js/Types/generate-app-types-ts.js';
+import generateAppVvClassesTypesTs from '../generators/jetstream/resources/js/Types/generate-app-vv-classes-types-ts.js';
+import generateTsconfigJson from '../generators/jetstream/generate-tsconfig-json.js';
+import generateViteTsxDts from '../generators/jetstream/resources/js/generate-vite-tsx-d-ts.js';
+import generateViteWindowDts from '../generators/jetstream/resources/js/generate-vite-window-d-ts.js';
+import generateZiggyTs from '../generators/jetstream/resources/js/generate-ziggy-ts.js';
+// import vue ecosystem app typed VILT DS files
+import generateBootstrapTs from '../generators/jetstream/resources/js/generate-bootstrap-ts.js';
+// import tailwind app typed files
+import generateTailwindConfigTs from '../generators/jetstream/generate-tailwind-config-ts.js';
+import generateAppTs from '../generators/jetstream/resources/js/generate-app-ts.js';
+import generateViteConfigTs from '../generators/jetstream/generate-vite-config-ts.js';
+// import VueVentus and VueVentus dependency app files
+import viltDsDefaultColorPalette from '../data/colors/vilt-ds-default-tailwind-color-palette.js';
+import generateAppVvTs from '../generators/jetstream/resources/js/generate-app-vv-ts.js';
+import generateAppVvClassesTs from '../generators/jetstream/resources/js/generate-app-vv-classes-ts.js';
+import generateJetstreamGsapTs from '../generators/jetstream/resources/js/generate-jetstream-gsap-ts.js';
+// import laravel blade files
+import generateAppBladePhp from '../generators/jetstream/resources/views/generate-app-blade-php.js';
+// import laravel jetstream layout component files
+import generateAppLayoutVue from '../generators/jetstream/resources/js/Layouts/generate-app-layout-vue.js';
+// import laravel php files for VILT DS
+import generateJetstreamPhp from '../generators/jetstream/config/generate-jetstream-php.js';
+import generateWebPhp from '../generators/jetstream/routes/generate-web-php.js';
+// import laravel and vueventus updatable files to be installed
+import vueventus from '../updaters/vueventus.js';
+import jetstreamBaseComponents from '../updaters/jetstream-base-components.js';
+import jetstreamApi from '../updaters/jetstream-api.js';
+import jetstreamAuth from '../updaters/jetstream-auth.js';
+import jetstreamProfile from '../updaters/jetstream-profile.js';
+import jetstreamTeams from '../updaters/jetstream-teams.js';
+import jetstreamBasePages from '../updaters/jetstream-base-pages.js';
+// import suggested VS Code Extensions for VILT DS
+import viltDsVscodeExtensions from '../data/ide/vilt-ds-vscode-extensions.js';
+export default function () {
+    // install client side deps and package scripts/mods for VILT DS and VueVentus
+    installJetstreamNpmDeps();
+    installJetstreamPackageJsonMods();
+    // update the app gitignore file for VILT DS
+    installFile('/.gitignore', generateGitignore());
+    // install typescript specific app files
+    installFile('/resources/js/Types/app-colors-types.ts', generateAppColorsTypesTs());
+    installFile('/resources/js/Types/app-jetstream-types.ts', generateAppJetstreamTypesTs());
+    installFile('/resources/js/Types/app-types.ts', generateAppTypesTs());
+    installFile('/resources/js/Types/app-vv-classes-types.ts', generateAppVvClassesTypesTs());
+    installFile('/tsconfig.json', generateTsconfigJson());
+    installFile('/resources/js/vite-tsx.d.ts', generateViteTsxDts());
+    installFile('/resources/js/vite-window.d.ts', generateViteWindowDts());
+    installFile('/resources/js/ziggy.ts', generateZiggyTs());
+    // install vue ecosystem app typed VILT DS files
+    installFile('/resources/js/bootstrap.ts', generateBootstrapTs());
+    installFile('/resources/js/app.ts', generateAppTs());
+    installFile('/vite.config.ts', generateViteConfigTs());
+    // install tailwind app typed files
+    installFile('/tailwind.config.ts', generateTailwindConfigTs());
+    // install VueVentus and VueVentus dependency app files
+    installFile('/resources/js/app.colors.json', JSON.stringify(viltDsDefaultColorPalette, null, 4));
+    installFile('/resources/js/app.vv.ts', generateAppVvTs());
+    installFile('/resources/js/app.vv.classes.ts', generateAppVvClassesTs());
+    installFile('/resources/js/gsap.ts', generateJetstreamGsapTs());
+    // install laravel blade files
+    installFile('/resources/views/app.blade.php', generateAppBladePhp());
+    // install laravel jetstream layout component files
+    installFile('/resources/js/Layouts/AppLayout.vue', generateAppLayoutVue());
+    // update laravel php files for VILT DS
+    installFile('/config/jetstream.php', generateJetstreamPhp());
+    installFile('/routes/web.php', generateWebPhp());
+    // install laravel and vueventus updatable files
+    vueventus();
+    jetstreamBaseComponents();
+    jetstreamApi();
+    jetstreamAuth();
+    jetstreamProfile();
+    jetstreamTeams();
+    jetstreamBasePages();
+    // install suggested VS Code Extensions for VILT DS
+    installFile('/.vscode/extensions.json', JSON.stringify(viltDsVscodeExtensions, null, 4));
+    console.log(' ');
+    // delete javascript files now replaced by typescript files
+    removeFile('/vite.config.js');
+    removeFile('/tailwind.config.js');
+    removeFile('/tailwind.config.js');
+    removeFile('/resources/js/bootstrap.js');
+    removeFile('/resources/js/app.js');
+}
+//# sourceMappingURL=install-vilt-ds-jetstream.js.map
