830b4a606be103f87ac5fd533b544a7418a573be
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-08-29 14:14:04 -0400

    patching incorrect filename/method to fix output of UpdateTeamNameForm.vue in ./src/updaters/jetstream-team.ts

--- src/updaters/jetstream-teams.ts
+++ src/updaters/jetstream-teams.ts
@@ -5,7 +5,7 @@ import installFile from '../helpers/install-file.js'
 import generateCreateTeamFormVue from '../generators/jetstream/resources/js/Pages/Teams/Partials/generate-create-team-form-vue.js'
 import generateDeleteTeamFormVue from '../generators/jetstream/resources/js/Pages/Teams/Partials/generate-delete-team-form-vue.js'
 import generateTeamMemberManagerVue from '../generators/jetstream/resources/js/Pages/Teams/Partials/generate-team-member-manager-vue.js'
-import generateUpdateNameFormVue from '../generators/jetstream/resources/js/Pages/Teams/Partials/generate-update-team-name-form-vue.js'
+import generateUpdateTeamNameFormVue from '../generators/jetstream/resources/js/Pages/Teams/Partials/generate-update-team-name-form-vue.js'
 import generateTeamsCreateVue from '../generators/jetstream/resources/js/Pages/Teams/generate-teams-create-vue.js'
 import generateTeamsShowVue from '../generators/jetstream/resources/js/Pages/Teams/generate-teams-show-vue.js'
 
@@ -14,7 +14,7 @@ export default function (): void {
     installFile( '/resources/js/Pages/Teams/Partials/CreateTeamForm.vue', generateCreateTeamFormVue() )
     installFile( '/resources/js/Pages/Teams/Partials/DeleteTeamForm.vue', generateDeleteTeamFormVue() )
     installFile( '/resources/js/Pages/Teams/Partials/TeamMemberManager.vue', generateTeamMemberManagerVue() )
-    installFile( '/resources/js/Pages/Teams/Partials/UpdateNameForm.vue', generateUpdateNameFormVue() )
+    installFile( '/resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue', generateUpdateTeamNameFormVue() )
     installFile( '/resources/js/Pages/Teams/Create.vue', generateTeamsCreateVue() )
     installFile( '/resources/js/Pages/Teams/Show.vue', generateTeamsShowVue() )
 
