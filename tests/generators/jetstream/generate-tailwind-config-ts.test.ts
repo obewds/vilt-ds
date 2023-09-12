01000f40762ea50dda9459629db4835fbfe2a428
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-12 12:57:33 -0400

    fixing test description's incorrect file path

--- tests/generators/jetstream/generate-tailwind-config-ts.test.ts
+++ tests/generators/jetstream/generate-tailwind-config-ts.test.ts
@@ -3,7 +3,7 @@
 import { describe, expect, test } from '@jest/globals'
 import generateTailwindConfigTs from '../../../src/generators/jetstream/generate-tailwind-config-ts'
 
-describe('./src/generators/jetstream/vscode/generate-tailwind-config-ts.test module', () => {
+describe('./src/generators/jetstream/generate-tailwind-config-ts.test module', () => {
 
     test('module can be imported and a value can be accessed successfully', () => {
 
