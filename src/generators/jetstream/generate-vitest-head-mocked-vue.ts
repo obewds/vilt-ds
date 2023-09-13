90d6d369492142099d3f264aac5035e48ded3531
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-13 03:30:26 -0400

    adding new ./src/generators/jetstream/generate-vitest-head-mocked-vue.ts

---
+++ src/generators/jetstream/generate-vitest-head-mocked-vue.ts
@@ -0,0 +1,14 @@
+// ./src/generators/jetstream/generate-vitest-head-mocked-vue.ts
+
+export default function (): string {
+
+return `<!-- ./VitestHeadMocked.vue -->
+
+<template>
+    <div>
+        Mocked head
+    </div>
+</template>
+`;
+
+}
