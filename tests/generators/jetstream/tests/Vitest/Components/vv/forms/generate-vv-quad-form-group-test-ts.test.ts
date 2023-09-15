7720400a1c51032080f749624c73937ba5960284
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-15 12:37:42 -0400

    adding new ./tests/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts.test.ts

---
+++ tests/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts.test.ts
@@ -0,0 +1,20 @@
+// ./tests/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import generateVvFormGroupTestTs from '../../../../../../../../src/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts'
+
+describe('./src/generators/jetstream/tests/Vitest/Components/generate-vv-quad-form-group-test-ts module', () => {
+
+    test('module can be imported and a value can be accessed successfully', () => {
+
+        expect(generateVvFormGroupTestTs).toBeTruthy()
+
+    })
+    
+    test('module default value is a valid string', () => {
+
+        expect(typeof generateVvFormGroupTestTs() === 'string').toBe(true)
+
+    })
+
+})
