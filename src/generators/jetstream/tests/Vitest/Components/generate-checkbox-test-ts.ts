890cab3a4c2ef592f5c42667c0b4ea6bbd25542f
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:18:25 -0400

    adding new generate-checkbox-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-checkbox-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-checkbox-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/Checkbox.test.ts
+
+import { mount } from '@vue/test-utils'
+import Checkbox from '../../../resources/js/Components/Checkbox.vue'
+
+
+test('Checkbox.vue component imports successfully', async () => {
+
+    expect(Checkbox).toBeTruthy()
+
+})
+`;
+
+}
