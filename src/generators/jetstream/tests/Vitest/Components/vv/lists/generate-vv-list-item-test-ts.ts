096494dfe6a2ddee4e42036ca6584611f9b1820a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:29:23 -0400

    adding new generate-vv-list-item-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/vv/lists/generate-vv-list-item-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/vv/lists/generate-vv-list-item-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/vv/lists/VvListItem.test.ts
+
+import { mount } from '@vue/test-utils'
+import VvListItem from '../../../../../resources/js/Components/vv/lists/VvListItem.vue'
+
+
+test('VvListItem.test.vue component imports successfully', async () => {
+
+    expect(VvListItem).toBeTruthy()
+
+})
+`;
+
+}
