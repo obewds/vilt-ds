3a505cd54150991aaf86d6a1bb667e22c9446de2
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:18:03 -0400

    adding new generate-authentication-card-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-authentication-card-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-authentication-card-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/AuthenticationCard.test.ts
+
+import { mount } from '@vue/test-utils'
+import AuthenticationCard from '../../../resources/js/Components/AuthenticationCard.vue'
+
+
+test('AuthenticationCard.vue component imports successfully', async () => {
+
+    expect(AuthenticationCard).toBeTruthy()
+
+})
+`;
+
+}
