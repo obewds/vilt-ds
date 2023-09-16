dabc58b0989458fd82960c5251494f595a051f25
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:25:46 -0400

    adding new generate-vv-scroll-up-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/vv/buttons/generate-vv-scroll-up-test-ts.ts
@@ -0,0 +1,62 @@
+// ./src/generators/jetstream/tests/Vitest/Components/vv/buttons/generate-vv-scroll-up-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/vv/buttons/VvScrollUp.test.ts
+
+import { mount } from '@vue/test-utils'
+import VvScrollUp from '../../../../../resources/js/Components/vv/buttons/VvScrollUp.vue'
+
+
+test('VvScrollUp.test.vue component imports successfully', async () => {
+
+    expect(VvScrollUp).toBeTruthy()
+
+})
+
+
+test('VvScrollUp.vue component returns markup if passed a true value for the isVisible prop', async () => {
+
+    const wrapper = mount(VvScrollUp, {
+        props: {
+            isVisible: true
+        },
+    })
+
+    expect(wrapper.html()).toContain('button')
+
+})
+
+
+test('VvScrollUp.vue component is impacted by a click event as expected', async () => {
+
+    const wrapper = mount(VvScrollUp, {
+        props: {
+            isVisible: true
+        },
+    })
+
+    window.dispatchEvent(new CustomEvent('scroll', { detail: 2000 }))
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.html()).toContain('button')
+
+})
+
+
+test('VvScrollUp.vue component debug mode returns an expected result', async () => {
+
+    const wrapper = mount(VvScrollUp, {
+        props: {
+            debug: true,
+            isVisible: true,
+        },
+    })
+
+    expect(wrapper.html()).toContain('data-vv-scroll-up-')
+
+})
+`;
+
+}
