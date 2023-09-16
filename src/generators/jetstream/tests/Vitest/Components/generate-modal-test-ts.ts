896503bcd070abb1b2363deabb8498135c662e02
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:23:11 -0400

    adding new generate-modal-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-modal-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-modal-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/Modal.test.ts
+
+import { mount } from '@vue/test-utils'
+import Modal from '../../../resources/js/Components/Modal.vue'
+
+
+test('Modal.vue component imports successfully', async () => {
+
+    expect(Modal).toBeTruthy()
+
+})
+`;
+
+}
