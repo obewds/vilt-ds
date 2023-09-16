28a98cc273c2f0e49b776f3d79547d3ca562d547
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:29:45 -0400

    adding new generate-vv-listbox-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/vv/selects/generate-vv-listbox-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/vv/selects/generate-vv-listbox-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/vv/selects/VvListbox.test.ts
+
+import { mount } from '@vue/test-utils'
+import VvListbox from '../../../../../resources/js/Components/vv/selects/VvListbox.vue'
+
+
+test('VvListbox.test.vue component imports successfully', async () => {
+
+    expect(VvListbox).toBeTruthy()
+
+})
+`;
+
+}
