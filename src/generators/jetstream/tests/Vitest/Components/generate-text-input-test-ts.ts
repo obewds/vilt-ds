8c4f433f5b1636045aa981b2eee724a121aeb3d9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:24:29 -0400

    adding new generate-text-input-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-text-input-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-text-input-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/TextInput.test.ts
+
+import { mount } from '@vue/test-utils'
+import TextInput from '../../../resources/js/Components/TextInput.vue'
+
+
+test('TextInput.vue component imports successfully', async () => {
+
+    expect(TextInput).toBeTruthy()
+
+})
+`;
+
+}
