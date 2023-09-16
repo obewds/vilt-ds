2fbfb441b6f05b837abe4d30f1ce4eaccefd7cae
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:03:44 -0400

    adding new generate-nav-link-test-ts.test.ts

---
+++ tests/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts.test.ts
@@ -0,0 +1,20 @@
+// ./tests/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import generateYyyyyyTest from '../../../../../../src/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts'
+
+describe('./src/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts module', () => {
+
+    test('module can be imported and a value can be accessed successfully', () => {
+
+        expect(generateYyyyyyTest).toBeTruthy()
+
+    })
+    
+    test('module default value is a valid string', () => {
+
+        expect(typeof generateYyyyyyTest() === 'string').toBe(true)
+
+    })
+
+})
