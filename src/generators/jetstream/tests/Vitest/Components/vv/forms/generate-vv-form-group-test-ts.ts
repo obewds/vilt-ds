b07d8168f1165cae169d0a35baf5ec0831e2ebec
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:26:27 -0400

    adding new generate-vv-form-group-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-form-group-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-form-group-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/vv/forms/VvFormGroup.test.ts
+
+import { mount } from '@vue/test-utils'
+import VvFormGroup from '../../../../../resources/js/Components/vv/forms/VvFormGroup.vue'
+
+
+test('VvFormGroup.test.vue component imports successfully', async () => {
+
+    expect(VvFormGroup).toBeTruthy()
+
+})
+`;
+
+}
