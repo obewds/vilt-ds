ed3265b45641b47f91f06f76bd1caf373c87d911
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:22:03 -0400

    adding new generate-dropdown-link-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-dropdown-link-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-dropdown-link-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/DropdownLink.test.ts
+
+import { mount } from '@vue/test-utils'
+import DropdownLink from '../../../resources/js/Components/DropdownLink.vue'
+
+
+test('DropdownLink.vue component imports successfully', async () => {
+
+    expect(DropdownLink).toBeTruthy()
+
+})
+`;
+
+}
