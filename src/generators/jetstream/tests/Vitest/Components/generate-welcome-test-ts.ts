28cacad381dba51bd60f62fb1e0833b3cb5c1149
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:24:39 -0400

    adding new generate-welcome-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-welcome-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-welcome-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/Welcome.test.ts
+
+import { mount } from '@vue/test-utils'
+import Welcome from '../../../resources/js/Components/Welcome.vue'
+
+
+test('Welcome.vue component imports successfully', async () => {
+
+    expect(Welcome).toBeTruthy()
+
+})
+`;
+
+}
