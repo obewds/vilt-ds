0b956721ef06fa2864e449ce91e89b882098e9d4
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:24:11 -0400

    adding new generate-section-border-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-section-border-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-section-border-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/SectionBorder.test.ts
+
+import { mount } from '@vue/test-utils'
+import SectionBorder from '../../../resources/js/Components/SectionBorder.vue'
+
+
+test('SectionBorder.vue component imports successfully', async () => {
+
+    expect(SectionBorder).toBeTruthy()
+
+})
+`;
+
+}
