17d2e44ce320e191e0d028fb3cb1f245669cfc59
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:21:54 -0400

    adding new generate-dialog-modal-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-dialog-modal-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-dialog-modal-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/DialogModal.test.ts
+
+import { mount } from '@vue/test-utils'
+import DialogModal from '../../../resources/js/Components/DialogModal.vue'
+
+
+test('DialogModal.vue component imports successfully', async () => {
+
+    expect(DialogModal).toBeTruthy()
+
+})
+`;
+
+}
