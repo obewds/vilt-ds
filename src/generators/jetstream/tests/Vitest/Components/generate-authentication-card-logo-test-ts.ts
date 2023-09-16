1324fc861f1aa7f7bc7c38f7fe804b2cbb9611ab
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:17:53 -0400

    adding new generate-authentication-card-logo-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-authentication-card-logo-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-authentication-card-logo-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/AuthenticationCardLogo.test.ts
+
+import { mount } from '@vue/test-utils'
+import AuthenticationCardLogo from '../../../resources/js/Components/AuthenticationCardLogo.vue'
+
+
+test('AuthenticationCardLogo.vue component imports successfully', async () => {
+
+    expect(AuthenticationCardLogo).toBeTruthy()
+
+})
+`;
+
+}
