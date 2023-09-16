ae3bc563eb30d4b386ef03566d97d1c8f20e0fe9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 15:30:31 -0400

    adding new ./tests/updaters/vueventus-tests.test.ts

---
+++ tests/updaters/vueventus-tests.test.ts
@@ -0,0 +1,15 @@
+// ./tests/updaters/vueventus-tests.test.ts
+
+import { describe, expect, test } from '@jest/globals'
+import vueventusTests from '../../src/updaters/vueventus-tests'
+
+
+describe('./src/updaters/vueventus-tests module', () => {
+
+    test('module can be imported successfully', () => {
+
+        expect(vueventusTests).toBeTruthy()
+
+    })
+
+})
