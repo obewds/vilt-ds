d5cf3853a3c765b0dc8f32dd8c8dde064495b717
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:26:38 -0400

    adding new generate-vv-quad-form-group-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/vv/forms/generate-vv-quad-form-group-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/vv/forms/VvQuadFormGroup.test.ts
+
+import { mount } from '@vue/test-utils'
+import VvQuadFormGroup from '../../../../../resources/js/Components/vv/forms/VvQuadFormGroup.vue'
+
+
+test('VvQuadFormGroup.test.vue component imports successfully', async () => {
+
+    expect(VvQuadFormGroup).toBeTruthy()
+
+})
+`;
+
+}
