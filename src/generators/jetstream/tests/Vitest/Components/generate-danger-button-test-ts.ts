4cc80c6f82186ffb629c05dc33d65ca84b05c432
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:19:23 -0400

    adding new generate-danger-button-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-danger-button-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-danger-button-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/DangerButton.test.ts
+
+import { mount } from '@vue/test-utils'
+import DangerButton from '../../../resources/js/Components/DangerButton.vue'
+
+
+test('DangerButton.vue component imports successfully', async () => {
+
+    expect(DangerButton).toBeTruthy()
+
+})
+`;
+
+}
