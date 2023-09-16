d4c92f559b239df1a8621aac12a948794d7c7603
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-16 00:23:21 -0400

    adding new generate-nav-link-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts.ts
@@ -0,0 +1,18 @@
+// ./src/generators/jetstream/tests/Vitest/Components/generate-nav-link-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Components/NavLink.test.ts
+
+import { mount } from '@vue/test-utils'
+import NavLink from '../../../resources/js/Components/NavLink.vue'
+
+
+test('NavLink.vue component imports successfully', async () => {
+
+    expect(NavLink).toBeTruthy()
+
+})
+`;
+
+}
