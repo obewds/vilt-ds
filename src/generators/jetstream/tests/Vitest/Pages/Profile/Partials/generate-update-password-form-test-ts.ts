47716da29a9930810bb7b87091e9632a4902cfcf
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-17 22:15:53 -0400

    adding new ./src/generators/jetstream/tests/Vitest/Pages/Profile/Partials/generate-update-password-form-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Pages/Profile/Partials/generate-update-password-form-test-ts.ts
@@ -0,0 +1,17 @@
+// ./src/generators/jetstream/tests/Vitest/Pages/Profile/Partials/generate-update-password-form-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Vitest/Pages/Profile/UpdatePasswordForm.test.ts
+
+import { mount } from '@vue/test-utils'
+import UpdatePasswordForm from '../../../../../resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue'
+
+test('Profile/UpdatePasswordForm.vue component imports successfully', async () => {
+
+    expect(UpdatePasswordForm).toBeTruthy()
+
+})
+`;
+
+}
