e93396f8a0be7b4d0c6bd2d9bfcefc2a4019f01b
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-17 21:53:14 -0400

    adding new ./src/generators/jetstream/tests/Vitest/Pages/Auth/generate-two-factor-challenge-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Pages/Auth/generate-two-factor-challenge-test-ts.ts
@@ -0,0 +1,17 @@
+// ./src/generators/jetstream/tests/Vitest/Pages/Auth/generate-two-factor-challenge-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Vitest/Pages/Auth/TwoFactorChallenge.test.ts
+
+import { mount } from '@vue/test-utils'
+import TwoFactorChallenge from '../../../../resources/js/Pages/Auth/TwoFactorChallenge.vue'
+
+test('Auth/TwoFactorChallenge.vue component imports successfully', async () => {
+
+    expect(TwoFactorChallenge).toBeTruthy()
+
+})
+`;
+
+}
