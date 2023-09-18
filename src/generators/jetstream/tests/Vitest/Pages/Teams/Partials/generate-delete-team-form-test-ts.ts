cf2efddebc2b942e55414816fbe3ef46306a016c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-17 22:16:55 -0400

    adding new ./src/generators/jetstream/tests/Vitest/Pages/Teams/Partials/generate-delete-team-form-test-ts.ts

---
+++ src/generators/jetstream/tests/Vitest/Pages/Teams/Partials/generate-delete-team-form-test-ts.ts
@@ -0,0 +1,17 @@
+// ./src/generators/jetstream/tests/Vitest/Pages/Teams/Partials/generate-delete-team-form-test-ts.ts
+
+export default function (): string {
+
+return `// ./tests/Vitest/Pages/Teams/DeleteTeamForm.test.ts
+
+import { mount } from '@vue/test-utils'
+import DeleteTeamForm from '../../../../../resources/js/Pages/Teams/Partials/DeleteTeamForm.vue'
+
+test('DeleteTeamForm.vue component imports successfully', async () => {
+
+    expect(DeleteTeamForm).toBeTruthy()
+
+})
+`;
+
+}
