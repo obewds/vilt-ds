d7e5449d46d8cdc55c00266a43d790a1e38ba95e
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-19 22:55:24 -0400

    fixing incorrect output code comment file path (missing Vitest dir)

--- src/generators/jetstream/tests/Vitest/Components/generate-responsive-nav-link-test-ts.ts
+++ src/generators/jetstream/tests/Vitest/Components/generate-responsive-nav-link-test-ts.ts
@@ -2,7 +2,7 @@
 
 export default function (): string {
 
-return `// ./tests/Components/ResponsiveNavLink.test.ts
+return `// ./tests/Vitest/Components/ResponsiveNavLink.test.ts
 
 import { mount } from '@vue/test-utils'
 import ResponsiveNavLink from '../../../resources/js/Components/ResponsiveNavLink.vue'
