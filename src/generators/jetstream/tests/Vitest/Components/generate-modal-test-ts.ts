d0a849ba73b755924570868b69b5ba4fc4a263da
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-09-19 22:55:14 -0400

    fixing incorrect output code comment file path (missing Vitest dir)

--- src/generators/jetstream/tests/Vitest/Components/generate-modal-test-ts.ts
+++ src/generators/jetstream/tests/Vitest/Components/generate-modal-test-ts.ts
@@ -2,7 +2,7 @@
 
 export default function (): string {
 
-return `// ./tests/Components/Modal.test.ts
+return `// ./tests/Vitest/Components/Modal.test.ts
 
 import { mount } from '@vue/test-utils'
 import Modal from '../../../resources/js/Components/Modal.vue'
