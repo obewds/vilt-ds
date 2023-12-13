9cfe5ec8065ae0b5e08638aed99b48d78e55dbdc
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2023-12-13 18:40:30 -0500

    changing generator for vv-anchor back to normal code output as puff.js project is not using correct js format tor node to run :(

--- src/generators/jetstream/resources/js/Components/vv/anchors/generate-vv-anchor-vue.ts
+++ src/generators/jetstream/resources/js/Components/vv/anchors/generate-vv-anchor-vue.ts
@@ -1,16 +1,16 @@
 // ./src/generators/jetstream/resources/js/Components/vv/anchors/generate-vv-anchor-vue.ts
 
-import { generateVvAnchorVue } from '@obewds/puff-js'
+// import { generateVvAnchorVue } from '@obewds/puff-js'
 
 export default function (): string {
 
-    const output = generateVvAnchorVue({
-        filePathComment: './resources/js/Components/vv/anchors/VvAnchor.vue',
-        appVvFilePath: '../../../app.vv',
-    })
+    // const output = generateVvAnchorVue({
+    //     filePathComment: './resources/js/Components/vv/anchors/VvAnchor.vue',
+    //     appVvFilePath: '../../../app.vv',
+    // })
 
-    return output
-/*
+    // return output
+
 return `<!-- ./resources/js/Components/vv/anchors/VvAnchor.vue -->
 
 <script lang="ts">
@@ -101,6 +101,5 @@ return `<!-- ./resources/js/Components/vv/anchors/VvAnchor.vue -->
 
 </template>
 `;
-*/
 
 }
